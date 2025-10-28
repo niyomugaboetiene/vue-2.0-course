8bee278c326c235ef419ed9ff7dde6f4b855e22b
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-28 20:07:12 +0200

    integration of tailwindcss

--- src/components/directives.vue
+++ src/components/directives.vue
@@ -1,1 +1,3 @@
 <!-- -v-bind -> binds an attribute dynamically -->
+ <!-- means Dynamically sets attribute on an element like (src, class, ref)  using vue variable -->
+  <!-- syntax :attribute="expression" -->

b5cb102f59c67bcbd95f79da720ddeac82712e1f
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-28 19:56:07 +0200

    integration of tailwindcss

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,6 +1,6 @@
 <template>
-   <div class="ms-[700px] mt-[100px]">
-    <p class="text-center me-[900px]">{{ count }}</p>
+   <div class="ms-[700px] mt-[100px] p-7 shadow-2xl w-[15%]">
+    <p class="text-center me-[1010px]">{{ count }}</p>
      <button @click="increment">Increment</button>
      <button @click="reset">Reset</button>
      <button @click="decrement">Decrement</button>

2ceca25cade0e9cbf8c040a64fb9b2675e91f69f
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-28 20:01:47 +0200

    integration of tailwindcss

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,7 +1,7 @@
 <template>
    <div class="ms-[700px] mt-[100px] p-7 shadow-2xl w-[20%] rounded-xl">
-    <p class="text-center me-[30px] font-bold">{{ count }}</p>
-    <div class="flex space-x-6">
+    <p class="text-center me-[10px] font-bold mb-6">{{ count }}</p>
+    <div class="flex space-x-6 ms-4">
         <button @click="increment" class="bg-blue-400 px-2 py-2 text-white rounded-lg hover:bg-blue-500 transition duration-200">Increment</button>
         <button @click="reset" class="bg-red-400 px-2 py-2 text-white rounded-lg hover:bg-red-500 transition duration-200">Reset</button>
         <button @click="decrement" class="bg-green-400 px-2 py-2 text-white rounded-lg hover:bg-green-500 transition duration-200">Decrement</button>

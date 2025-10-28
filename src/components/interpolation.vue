82eac028fc5c63f5f7d38832eaf3ce72cb959a65
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-28 19:35:59 +0200

    integration of tailwindcss

--- src/components/interpolation.vue
+++ src/components/interpolation.vue
@@ -1,1 +1,11 @@
 <!-- interpolation is way of displaying daynamic data like (variable or expression) directly inside your HTML template -->
+ <template>
+    <div>
+        <h1>{{  message  }}</h1>
+    </div>
+ </template>
+
+ <script setup>
+     import { ref } from 'vue';
+
+</script>

06bfeea16c9b6f3fc9e934cd208344aab54afb16
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-28 19:31:40 +0200

    integration of tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="ms-[700px] p-5  mt-[100px] w-[400px] rounded-lg shadow-2xl">
+  <div class="ms-[700px] p-8 text-gray-600  mt-[100px] w-[400px] rounded-lg shadow-2xl">
     <h1 class="text-center text-lg font-bold">Welcome to My Vue App</h1>
-    <p class="">Start building your component here!</p>
+    <p class="text-center text-gray-500">Start building your component here!</p>
     <HelloWorld />
   </div>
 </template>

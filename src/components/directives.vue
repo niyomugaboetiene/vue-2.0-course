c52ee1fc953f0074a82c99cf911f716d2b69bc6d
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-28 20:08:36 +0200

    integration of tailwindcss

--- src/components/directives.vue
+++ src/components/directives.vue
@@ -1,3 +1,11 @@
 <!-- -v-bind -> binds an attribute dynamically -->
  <!-- means Dynamically sets attribute on an element like (src, class, ref)  using vue variable -->
   <!-- syntax :attribute="expression" -->
+
+  <template>
+    <div>
+        <img :src="imageUrl" alt="Dynamic image" />
+        <p :class="textClass">Hello Vue!</p>
+    </div>
+  </template>
+  

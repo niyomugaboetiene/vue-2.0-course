0fdbb2015a6d56179cde1d5b77ab7cb36318e259
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-28 19:37:49 +0200

    integration of tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
-  <div class="ms-[700px] p-8 text-gray-600  mt-[100px] w-[400px] rounded-lg shadow-2xl">
+  <!-- <div class="ms-[700px] p-8 text-gray-600  mt-[100px] w-[400px] rounded-lg shadow-2xl">
     <h1 class="text-center text-lg font-bold">Welcome to My Vue App</h1>
     <p class="text-center text-gray-500">Start building your component here!</p>
     <HelloWorld />
+  </div> -->
+
+  <div>
+    <Interpolation />
   </div>
 </template>
 
 <script setup>
  import HelloWorld from './components/HelloWorld.vue';
+import Interpolation from './components/Interpolation.vue';
 </script>

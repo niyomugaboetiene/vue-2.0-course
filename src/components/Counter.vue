b0b36a8d8a322fa07797e2a8ac488c5107f04544
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-28 19:45:44 +0200

    integration of tailwindcss

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,7 +1,9 @@
 <template>
-   <div>
-    <p>{{ count }}</p>
+   <div class="ms-[700px] mt-[100px]">
+    <p class="text-center me-[900px]">{{ count }}</p>
      <button @click="increment">Increment</button>
+     <button @click="reset">Reset</button>
+     <button @click="decrement">Decrement</button>
    </div>     
 </template>
 
@@ -9,7 +11,16 @@
     import { ref } from 'vue';
 
     const count = ref(0);
+
     function increment() {
         count.value++
+    }
+
+    function reset() {
+        count.value = 0;
+    }
+
+    function decrement() {
+        count.value --;
     }
 </script>

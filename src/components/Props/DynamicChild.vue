2b57599f50d3758a2fae5b1d12a6b5f149a13130
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-01-20 10:17:53 +0200

    learning props in vue

--- src/components/Props/DynamicChild.vue
+++ src/components/Props/DynamicChild.vue
@@ -1,13 +1,13 @@
 <template>
+    <h1>{{ users.name }}</h1>
     <p v-for="skill in skills">{{ skill }}</p>
-    <p v-for="user in users">{{ user }}</p>
 </template>
 
 <script>
     export default {
         props: {
             skills: Array,
-            users: Array
+            users: Object
         }
     }
 </script>
